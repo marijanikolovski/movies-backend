c249ce721c4da9daa7554a9698527656a4d1fa65
marijanikolovski <m.kuzeljevic@gmail.com>
2023-01-16 09:35:47 +0100

    Task-CSDY-61-Movie entity

---
+++ app/Models/Movie.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'cover_image',
+        'genre_id'
+    ];
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+}
